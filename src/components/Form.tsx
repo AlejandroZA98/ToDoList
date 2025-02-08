658496af86ad264aee1e2c949ebf1bb544d1a537
Alejandro Zacarias <alez42676@gmail.com>
2025-02-08 10:50:15 -0600

    commit example

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,5 @@
+
+// EXAMPLE
 import { useState,Dispatch,useEffect} from "react"
 import { categories } from "../data/categories"
 import {ActivityActions,ActivityState} from  "../reducers/activity-reducer" 
